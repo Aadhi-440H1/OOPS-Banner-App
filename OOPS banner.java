88fa6cb87abf951ec1bfaa498b8f028100990718
Aadhi-440H1 <am8581@srmist.edu.in>
2026-02-16 09:28:37 +0530

    Add files via upload

---
+++ OOPS banner.java
@@ -0,0 +1,12 @@
+public class BannerOOPS {
+    public static void main(String[] args) {
+        // Each line prints O O P S side by side
+        System.out.println("  ***   " + "  ***   " + " *****  " + "  ***** ");
+        System.out.println(" *   *  " + " *   *  " + " *    * " + " *      ");
+        System.out.println(" *   *  " + " *   *  " + " *    * " + " *      ");
+        System.out.println(" *   *  " + " *   *  " + " *****  " + "  ***** ");
+        System.out.println(" *   *  " + " *   *  " + " *       " + "      * ");
+        System.out.println(" *   *  " + " *   *  " + " *       " + "      * ");
+        System.out.println("  ***   " + "  ***   " + " *       " + " *****  ");
+    }
+}
